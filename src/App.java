3245b270ec9691c9ac1d40e2c6168e7f797c69d0
Angel-D28 <angeld.diazp@uqvirtual.edu.co>
2026-03-12 23:48:40 -0500

    se hace el registrar estudiante y notas

--- src/App.java
+++ src/App.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class App {
+    static String estudiante;
+    static double nota1, nota2, nota3;
 
-    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        menu();
+        Scanner sc = new Scanner(System.in);
+        menu(sc);
 
     }
 
-    public static void menu() {
+    public static void menu(Scanner sc) {
         int opcion;
         do {
             System.out.println("==================================");
@@ -20,26 +22,56 @@ public class App {
             System.out.println("==================================");
             System.out.print("Seleccione una opción: ");
             opcion = sc.nextInt();
-            
+
+            sc.nextLine();
             switch (opcion) {
                 case 1:
-                    //registrarEstudiante();
-                    break;
-                
-                    case 2 :
-                    //mostrarEstudiante();
+                    registrarEstudiante(sc);
                     break;
 
-                    case 3:
-                    //calcularPromedioNotasEStudiante();
+                case 2:
+                    // mostrarEstudiante();
                     break;
-                    case 4:
-                        System.out.println("Saliendo del sistema....");
-                        break;
+
+                case 3:
+                    // calcularPromedioNotasEStudiante();
+                    break;
+                case 4:
+                    System.out.println("Saliendo del sistema....");
+                    break;
                 default:
                     System.out.println("Opcion no valida. Ingrese de nuevo");
                     break;
             }
         } while (opcion != 4);
+        
     }
+
+    public static void registrarEstudiante(Scanner sc) {
+        System.out.println("---Registro de Estudiante---");
+        do {
+            System.out.print("ingrese el nombre del estudiante: ");
+            estudiante = sc.nextLine();
+        } while (estudiante.isEmpty());
+        registrarNotas(sc, estudiante);
+    }
+
+    public static void registrarNotas(Scanner sc, String nombre) {
+        do {
+            System.out.print("ingrese Nota 1 de " + nombre + ": ");
+            nota1 = sc.nextDouble();
+        } while (nota1 > 5 || nota1 < 0);
+
+        do {
+            System.out.print("ingrese Nota 2 de " + nombre + ": ");
+            nota1 = sc.nextDouble();
+        } while (nota1 > 5 || nota1 < 0);
+
+        do {
+            System.out.print("ingrese Nota 3 de " + nombre + ": ");
+            nota1 = sc.nextDouble();
+        } while (nota1 > 5 || nota1 < 0);
+
+    }
+
 }
